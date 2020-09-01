f112bb90eff0d140139b9921c8f44cb19aca25ac
vmwhoami <vmwhoami@gmail.com>
2020-09-01 17:10:53 +0300

    Add show action for users

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -24,10 +24,14 @@ class UsersController < ApplicationController
     @user = User.find(params[:id])
     if @user.update(private_params)
       flash[:notice] = 'User was successfully updated'
-      redirect_to articles_path
+      redirect_to user_path
     else
       render :edit
     end
+  end
+
+  def show
+    @user = User.find(params[:id])
   end
 
   private
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,4 +1,9 @@
-# frozen_string_literal: true
 
 module ApplicationHelper
+def gravatar_for(obj)
+  gravata_id = Digest::MD5.hexdigest(obj.email.downcase)
+  image_src = "https://www.gravatar.com/avatar/#{gravata_id}"
+  image_tag(image_src,alt: obj.username, class: "rounded-circle")
+end
+
 end
