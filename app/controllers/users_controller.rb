7a39b36c846e0663f22e417d3b53e0429244aa7c
vmwhoami <vmwhoami@gmail.com>
2020-09-02 19:19:55 +0300

    Add user signup helper methods

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,8 @@
 
 class UsersController < ApplicationController
  
+before_action :find_user, only: [:edit, :update, :show]
+
  def index
   @users = User.paginate(page:params[:page], per_page: 3)
  end
@@ -22,11 +24,11 @@ class UsersController < ApplicationController
   end
 
   def edit
-    @user = User.find(params[:id])
+  
   end
 
   def update
-    @user = User.find(params[:id])
+  
     if @user.update(private_params)
       flash[:notice] = 'User was successfully updated'
       redirect_to user_path
@@ -36,11 +38,14 @@ class UsersController < ApplicationController
   end
 
   def show
-    @user = User.find(params[:id])
+  
     @user_articles = @user.articles.paginate(page:params[:page], per_page: 3)
   end
 
   private
+def find_user
+  @user = User.find(params[:id])
+end
 
   def private_params
     params.require(:user).permit(:username, :email, :password)
